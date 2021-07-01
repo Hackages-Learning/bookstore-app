45d8019f0860c73560441b1a3d5e7b71a6c09c96
Daan Paardekooper <daan@hackages.io>
2021-07-01 15:25:06 +0200

    capitalize pipe test

--- src/app/capitalize.pipe.spec.ts
+++ src/app/capitalize.pipe.spec.ts
@@ -5,4 +5,14 @@ describe('CapitalizePipe', () => {
     const pipe = new CapitalizePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should capitalize when transforming', () => {
+    const pipe = new CapitalizePipe();
+    expect(pipe.transform('value')).toBe('Value');
+  })
+
+  it('should trim when transforming', () => {
+    const pipe = new CapitalizePipe();
+    expect(pipe.transform(' Value ')).toBe('Value');
+  })
 });
